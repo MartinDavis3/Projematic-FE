c9ad8a064b88ee8d4023dde1cbd1ff92afba0d5d
Martin Davis <martin_davis3@yahoo.ca>
2020-03-20 00:03:49 -0600

    add additional tab syling

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,14 @@
 // Hide all elements with class="containerTab", except for the one that matches the clickable grid column
 function openTab(tabName, tabId) {
-    console.log(tabId);
     var i, x, y;
     x = document.getElementsByClassName("containerTab");
     y = document.getElementsByClassName("column");
     for (i = 0; i < x.length; i++) {
       x[i].style.display = "none";
       y[i].style.borderBottomStyle = "solid";
+      y[i].style.backgroundColor = "lightgray";
     }
     document.getElementById(tabName).style.display = "block";
     document.getElementById(tabId).style.borderBottomStyle = "none";
+    document.getElementById(tabId).style.backgroundColor = "white";
 } 
