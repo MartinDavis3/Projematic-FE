1ad4da9de33c97bdb940e7c4096c89803af2b608
Martin Davis <martin_davis3@yahoo.ca>
2020-03-24 16:50:49 -0600

    full combined desktop/mobile layout

--- js/mobile.js
+++ js/mobile.js
@@ -3,20 +3,28 @@ const screenWidthBreakpoint = 768
 // This function for expanding grid adapted from: w3schools How TO - Expanding Grid
 // https://www.w3schools.com/howto/howto_js_expanding_grid.asp
 // Hide all elements with class="containerTab", except for the one that matches the clickable grid column
-function openTab(tabName, tabId) {
+function openTab(sectionName, tabName) {
+  const secNames = ["preproject-section", "initiation-section", "planning-section", "execution-section", "closing-section"]
+  const secWidths = ["23.5vw", "23.5vw", "99vw", "75.5vw", "23.5vw"]
   var winWidth = window.innerWidth;
-  var i, x, y;
-  x = document.getElementsByClassName("flowchart-section");
-  y = document.getElementsByClassName("flowchart-tab");
+  var fcSection, fcTab;
+  fcSection = document.getElementsByClassName("flowchart-section");
+  fcTab = document.getElementsByClassName("flowchart-tab");
   if ( winWidth < screenWidthBreakpoint ) {
-    for (i = 0; i < x.length; i++) {
-      x[i].style.display = "none";
-      y[i].style.borderBottomStyle = "solid";
-      y[i].style.backgroundColor = "#c0c6d0";
+    for ( let i = 0; i < fcSection.length; i++) {
+      fcSection[i].style.display = "none";
+      fcTab[i].style.borderBottomStyle = "solid";
+      fcTab[i].style.borderBottomWidth = "1px";
+        fcTab[i].style.backgroundColor = "#c0c6d0";
     }
-    document.getElementById(tabName).style.display = "grid";
-    document.getElementById(tabId).style.borderBottomStyle = "none";
-    document.getElementById(tabId).style.backgroundColor = "#fcf7f8";
+    for ( let i = 0; i < 5; i++) {
+      if ( sectionName === secNames[i] ) {
+        document.getElementById("full-flowchart").style.gridTemplateColumns = secWidths[i]
+      }
+    }
+    document.getElementById(sectionName).style.display = "grid";
+    document.getElementById(tabName).style.borderBottomStyle = "none";
+    document.getElementById(tabName).style.backgroundColor = "#fcf7f8";
   }
 }
 
@@ -25,15 +33,16 @@ var lastWinWidth
 
 function handleResize() {
   var winWidth = window.innerWidth;
-  var i, x, y;
-  x = document.getElementsByClassName("flowchart-section");
-  y = document.getElementsByClassName("flowchart-tab");
+  var fcSection, fcTab;
+  fcSection = document.getElementsByClassName("flowchart-section");
+  fcTab = document.getElementsByClassName("flowchart-tab");
   if ( lastWinWidth < screenWidthBreakpoint && winWidth >= screenWidthBreakpoint ) {
-    for (i = 0; i < x.length; i++) {
-      x[i].style.display = "grid";
-      y[i].style.borderBottomStyle = "solid";
-      y[i].style.backgroundColor = "#fcf7f8";
+    for ( let i = 0; i < fcSection.length; i++) {
+      fcSection[i].style.display = "grid";
+      fcTab[i].style.borderBottomStyle = "solid";
+      fcTab[i].style.backgroundColor = "#fcf7f8";
     }  
+    document.getElementById("full-flowchart").style.gridTemplateColumns = "9.5vw 9.5vw 40vw 30.5vw 9.5vw"
   } else if ( lastWinWidth >= screenWidthBreakpoint && winWidth < screenWidthBreakpoint ) {
     openTab("preproject-section", "preproject-tab")
   }
