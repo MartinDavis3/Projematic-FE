410e2b68dd8b3b4ebc6834c1604ca5173a65b2e2
Martin Davis <martin_davis3@yahoo.ca>
2020-05-11 13:59:05 -0600

    refactor repeated popup code

--- js/scripts.js
+++ js/scripts.js
@@ -1,50 +1,32 @@
-var boxopen;
+const popupTargetId = [
+    'stakeholder-management-plan',
+    'stakeholder-register',
+    'communicate-with-stakeholders'
+];
 
-var mybox1 = document.getElementById( 'stakeholder-management-plan' );
-mybox1.addEventListener( 'click', function ( event ) {
-  
-    var popupDiv = document.getElementById('first-popup');
-    if (!boxopen) {
-        popupDiv.className = 'popup popup-visible';
-        boxopen = true;
-    }
-});
+const popupId = [
+    'first-popup',
+    'second-popup',
+    'third-popup'
+];
 
-function closeBox1(){
-    var popupDiv = document.getElementById('first-popup');
+var boxopen = false;
+
+for (let i = 0; i < popupTargetId.length; i++) {
+    let targetBox = document.getElementById( popupTargetId[i] );
+    targetBox.addEventListener( 'click', function ( event ) {
+    
+        let popupDiv = document.getElementById(popupId[i]);
+        if (!boxopen) {
+            popupDiv.className = 'popup popup-visible';
+            boxopen = true;
+        }
+    });
+}
+
+function closeBox(popupToClose){
+    var popupDiv = document.getElementById(popupToClose);
     popupDiv.className = 'popup popup-hidden';
     boxopen =false;
 }
 
-
-var mybox2 = document.getElementById( 'stakeholder-register' );
-mybox2.addEventListener( 'click', function ( event ) {
-    
-    var popupDiv = document.getElementById('second-popup');
-        if (!boxopen) {
-            popupDiv.className = 'popup popup-visible';
-            boxopen = true;
-        }
-});
-
-function closeBox2(){
-    var popupDiv = document.getElementById('second-popup');
-    popupDiv.className = 'popup popup-hidden';
-    boxopen = false;
-}
-
-var mybox3 = document.getElementById( 'communicate-with-stakeholders' );
-mybox3.addEventListener( 'click', function ( event ) {
-    var popupDiv = document.getElementById('third-popup');
-        if (!boxopen) {
-            popupDiv.className = 'popup popup-visible';
-            boxopen = true;
-        }
-    
-});
-
-function closeBox3(){
-    var popupDiv = document.getElementById('third-popup');
-    popupDiv.className = 'popup popup-hidden';
-    boxopen = false;
-}
